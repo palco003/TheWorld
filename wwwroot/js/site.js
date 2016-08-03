e67ac15c9965f3c3dd081905403298d2641a1773
Pablo Alcocer <palco003@fiu.edu>
2016-08-03 18:51:00 -0400

    hide sidebar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,26 @@
 // site.js
 (function (){
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Pablo J. Alcocer";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style.backgroundColor = "#888";
-    };
+    // var ele = $("#username");
+    // ele.text("Pablo J. Alcocer");
 
-    main.onmouseleave = function() {
-        main.style.backgroundColor = "";
-    };
+    // var main = $("#main");
+    // main.on("mouseenter", function() {
+    //     main.style = "backgroundColor: #888;";
+    // });
+
+    // main.on("mouseleave", function() {
+    //     main.style = "";
+    // });
+
+    // var menuItems = $("ul.menu li a");
+    // menuItems.on("click", function(){
+    //     var me = $(this);
+    //     alert(me.text());
+    // });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function(){
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+    });
 })();
