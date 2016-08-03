f626435bcd1ec81ccd115281f708e4ce42686c35
Pablo Alcocer <palco003@fiu.edu>
2016-08-03 18:58:57 -0400

    margin transition

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -22,5 +22,10 @@
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
     $("#sidebarToggle").on("click", function(){
         $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")){
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 })();
