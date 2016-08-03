56f8e0584d42cd0d85d1c3db6fe79668baeb24f1
Pablo Alcocer <palco003@fiu.edu>
2016-08-03 18:10:59 -0400

    added javascript

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+// site.js
+(function (){
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Pablo J. Alcocer";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function() {
+        main.style.backgroundColor = "";
+    };
+})();
