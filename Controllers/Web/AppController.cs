7294de6b13d32056fa2557b080d94cef01408b69
Pablo Alcocer <palco003@fiu.edu>
2016-08-04 23:45:39 -0400

    form validation

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -5,11 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+        public AppController(IMailService mailService, IConfigurationRoot config){
+            _mailService = mailService;
+            _config = config;
+        }
         public IActionResult Index(){
             return View();
         }
@@ -17,7 +25,10 @@ namespace TheWorld.Controllers.Web
             return View();
         }
         [HttpPost]
-        public IActionResult Contact(ContactViewModel model){
+        public IActionResult  Contact(ContactViewModel model){
+            if(ModelState.IsValid){
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+            }
             return View();
         }
         public IActionResult About(){
